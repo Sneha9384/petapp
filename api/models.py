39bfde8e2a9d6451ec91e3685cfa3625a03de20d
Sneha9384~ <snehashuruthi0058@gmail.com>
2025-03-04 14:51:16 +0530

    db updated

--- api/models.py
+++ api/models.py
@@ -54,32 +54,53 @@ class Pet(models.Model):
         ('female','female')
     ]
     user=models.ForeignKey(UserDetail, on_delete=models.CASCADE)
-    species_name=models.ForeignKey('Species', on_delete=models.CASCADE)
-    division_name=models.ForeignKey('Division', on_delete=models.CASCADE)
-    breed_name=models.ForeignKey('Breed', on_delete=models.CASCADE)
-    pet_name=models.CharField(max_length=50)
+    species=models.ForeignKey('Species', on_delete=models.CASCADE)
+    division=models.ForeignKey('Division', on_delete=models.CASCADE)
+    breed=models.ForeignKey('Breed', on_delete=models.CASCADE)
+    pet=models.CharField(max_length=50)
     age=models.IntegerField(default=None,null=True)
     gender=models.CharField( choices=gender_choices,max_length=50,default=None,null=True)
 
     def __str__(self):
-        return f'{self.user}- petname:{self.pet_name}'
+        return f'{self.user}- petname:{self.pet}'
 class Food(models.Model):
     breed_name=models.ForeignKey('Breed', on_delete=models.CASCADE)
-    food_type=[
+    food_choices=[
         ('dry food','dry food'),
         ('wet food','wet food'),
         ('treats','treats')
     ]
-    food_name=models.CharField(choices=food_type, max_length=50,default=None,null=True)
-    brand_name=models.CharField( max_length=50 ,default=None,null=True)
+
+    food_type=models.CharField(choices=food_choices, max_length=50,default=None,null=True)
+    brand=models.CharField( max_length=50 ,default=None,null=True)
+    food_name=models.CharField('Food', max_length=50,default=None,null=True)
+    price=models.IntegerField(default=None,null=True)
+    food_id=models.IntegerField(default=None,null=True)
     
     def __str__(self):
         return self.breed_name
+    def save(self,*args, **kwargs):
+        self.food_id= 'food'+self.food_id
+        super().save(**args, **kwargs)
+
 class Medicine(models.Model):
     breed_name=models.ForeignKey('Breed', on_delete=models.CASCADE)
+    medicine_choices=[
+        ('pharmacy','pharmacy'),
+        ('supplements','supplements')
+    ]
+    medicine_type=models.CharField(choices=medicine_choices, max_length=50)
+    medi_name=models.CharField( max_length=50,default=None,null=True)
+    brand=models.CharField( max_length=50 ,default=None,null=True)
+    price=models.IntegerField(default=None,null=True)
+    medicine_id=models.IntegerField(default=None,null=True)
 
     def __str__(self):
         return self.breed_name
+    def save(self,*args, **kwargs):
+        self.medicine_id= 'medicine'+self.medicine_id
+        super().save(**args, **kwargs)
+
 
 class Product(models.Model):
     breed_name=models.ForeignKey('Breed', on_delete=models.CASCADE)
@@ -90,5 +111,28 @@ class Product(models.Model):
     
     product_name=models.CharField( choices=product_type,max_length=50)
     product=models.CharField('Product',max_length=50,default=None,null=True)
+    brand=models.CharField( max_length=50 ,default=None,null=True)
+    price=models.IntegerField(default=None,null=True)
+    product_id=models.IntegerField(default=None,null=True)
     def __str__(self):
         return f'{self.breed_name}-{self.product_name}'
+    def save(self,*args, **kwargs):
+        self.product_id= 'product'+self.product_id
+        super().save(**args, **kwargs)
+
+class Ratings(models.Model):
+    user_name=models.ForeignKey( 'UserDetail', on_delete=models.CASCADE)
+    message=models.TextField()
+    star_choices=[
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5')
+
+    ]
+    star=models.CharField(choices=star_choices, max_length=50)
+    def __str__(self):
+        return self.user_name
+    
+    
