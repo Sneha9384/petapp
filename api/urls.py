ee7e61da9210cfa23b9dd42f3438339f5502cad2
Sneha9384~ <snehashuruthi0058@gmail.com>
2025-03-05 15:45:24 +0530

    developed

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path ,include
 from . import views
 
 urlpatterns = [
-    path("", views.index,name='index')
+    path("", views.index,name='index'),
+    path("user-detail/",views.user_detail_routes.as_view(),name='userdetail'),
+    path("user-detail/<int:id>",views.user_detail_routes.as_view(),name='userdetail')
 ]
--- petapp/settings.py
+++ petapp/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'api'
+    'api',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
